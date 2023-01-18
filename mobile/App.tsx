346b2bb580ac1ef15cc2b2937d41eaaa3bfa0228
LeonardoRCosta <rm94286@fiap.com.br>
2023-01-18 15:42:25 -0300

    finished class 2

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,5 @@
+import './src/lib/dayjs';
+
 import {
   Inter_400Regular,
   Inter_600SemiBold,
@@ -5,9 +7,10 @@ import {
   Inter_800ExtraBold,
   useFonts,
 } from '@expo-google-fonts/inter';
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,28 +23,13 @@ export default function App() {
   if (!fontsLoaded) return <Loading />;
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
-      </Text>
+    <>
+      <Home />
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold',
-  },
-});
